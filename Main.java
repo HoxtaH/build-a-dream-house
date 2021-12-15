5e2c9cbad3e6befe7dc840407b4f5686038da6af
Tyrell Hoxter <unclehox@Tyrells-MacBook-Pro.local>
2021-12-15 09:53:05 -0500

    Initital repo commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Starting!");
+    Picture p = new Picture();
+    p.draw();
+    System.out.println("Done Drawing.");
+  }
+}
